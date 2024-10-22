b8de96f377ea79b5cd8b381616918f884aca2ec9
Sujal <web.sujal@gmail.com>
2024-10-22 23:49:13 +0530

    fix bugs

--- src/component/header/header.jsx
+++ src/component/header/header.jsx
@@ -1,20 +1,21 @@
-import React, { useEffect, useState } from "react";
-import "./header.css";
-import logo from "../../assessts/images/logo.png";
-import SearchIcon from "@mui/icons-material/Search";
-import Select from "../SelectDrop/select";
-import axios from "axios";
-import LocationOnOutlinedIcon from "@mui/icons-material/LocationOnOutlined";
 import KeyboardArrowDownIcon from "@mui/icons-material/KeyboardArrowDown";
-import iconnheart from "../../assessts/images/heart.png";
-import cartimage from "../../assessts/images/cart.png";
-import user from "../../assessts/images/user.png";
-import Button from "@mui/material/Button";
+import LocationOnOutlinedIcon from "@mui/icons-material/LocationOnOutlined";
 import LocationSearchingOutlinedIcon from "@mui/icons-material/LocationSearchingOutlined";
-import Person2OutlinedIcon from "@mui/icons-material/Person2Outlined";
 import LoginOutlinedIcon from "@mui/icons-material/LoginOutlined";
+import Person2OutlinedIcon from "@mui/icons-material/Person2Outlined";
+import SearchIcon from "@mui/icons-material/Search";
 import SettingsOutlinedIcon from "@mui/icons-material/SettingsOutlined";
-import { ClickAwayListener } from '@mui/base/ClickAwayListener';
+import Button from "@mui/material/Button";
+import ClickAwayListener from "@mui/material/ClickAwayListener";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+
+import cartimage from "../../assessts/images/cart.png";
+import iconnheart from "../../assessts/images/heart.png";
+import logo from "../../assessts/images/logo.png";
+import user from "../../assessts/images/user.png";
+import Select from "../SelectDrop/select";
+import "./header.css";
 import Nav from "./nav/nav";
 
 const Header = () => {
@@ -53,102 +54,102 @@ const Header = () => {
 
   return (
     <>
-    <header>
-      <div className="container-fluid">
-        <div className="row">
-          <div className="col-sm-2">
-            <img src={logo} alt="Company Logo" />
-          </div>
+      <header>
+        <div className="container-fluid">
+          <div className="row">
+            <div className="col-sm-2">
+              <img src={logo} alt="Company Logo" />
+            </div>
 
-          <div className="col-sm-5">
-            <div className="headersearch d-flex align-item-center">
-              <Select
-                data={categories}
-                placeholder={"All Categories"}
-                icon={false}
-                arrow={<KeyboardArrowDownIcon className="arrow" />}
-              />
+            <div className="col-sm-5">
+              <div className="headersearch d-flex align-item-center">
+                <Select
+                  data={categories}
+                  placeholder={"All Categories"}
+                  icon={false}
+                  arrow={<KeyboardArrowDownIcon className="arrow" />}
+                />
 
-              <div className="search">
-                <input type="text" placeholder="Search for items..." />
-                <SearchIcon className="searchIcon cursor" />
+                <div className="search">
+                  <input type="text" placeholder="Search for items..." />
+                  <SearchIcon className="searchIcon cursor" />
+                </div>
               </div>
             </div>
-          </div>
 
-          <div className="col-sm-5">
-            <div className="ml-auto d-flex align-item-center">
-              <div className="countryWrapper">
-                <Select
-                  data={countryList} // Pass the country list as data
-                  placeholder="Your location"
-                  icon={<LocationOnOutlinedIcon style={{ opacity: "0.7" }} />}
-                  arrow={<KeyboardArrowDownIcon className="arrow2" />}
-                />
+            <div className="col-sm-5">
+              <div className="ml-auto d-flex align-item-center">
+                <div className="countryWrapper">
+                  <Select
+                    data={countryList} // Pass the country list as data
+                    placeholder="Your location"
+                    icon={<LocationOnOutlinedIcon style={{ opacity: "0.7" }} />}
+                    arrow={<KeyboardArrowDownIcon className="arrow2" />}
+                  />
+                </div>
+                <ClickAwayListener onClickAway={() => setIsopendropdown(false)}>
+                  <ul className="list list-inline mb-0 headerTabs">
+                    <li className="list-inline-item">
+                      <span>
+                        <img src={iconnheart} alt="Heart Icon" />
+                        Compare
+                      </span>
+                    </li>
+                    <li className="list-inline-item ">
+                      <span>
+                        <img src={iconnheart} alt="Wishlist Icon" />
+                        Wishlist
+                      </span>
+                    </li>
+                    <li className="list-inline-item ">
+                      <span>
+                        <img src={cartimage} alt="Cart Icon" />
+                        Cart
+                      </span>
+                    </li>
+                    <li className="list-inline-item ">
+                      <span onClick={() => setIsopendropdown(!isopendropdown)}>
+                        <img src={user} alt="User Icon" />
+                        Account
+                      </span>
+                      {isopendropdown && (
+                        <ul className="dropdownmenu">
+                          <li>
+                            <Button>
+                              <Person2OutlinedIcon />
+                              My Account
+                            </Button>
+                          </li>
+                          <li>
+                            <Button>
+                              <LocationSearchingOutlinedIcon />
+                              Order tracking
+                            </Button>{" "}
+                          </li>
+                          <li>
+                            <Button>
+                              <SettingsOutlinedIcon />
+                              Setting
+                            </Button>
+                          </li>
+                          <li>
+                            <Button>
+                              <LoginOutlinedIcon />
+                              Log out
+                            </Button>
+                          </li>
+                        </ul>
+                      )}
+                    </li>
+                  </ul>
+                </ClickAwayListener>
               </div>
-              <ClickAwayListener onClickAway={() => setIsopendropdown(false)}>
-                <ul className="list list-inline mb-0 headerTabs">
-                  <li className="list-inline-item">
-                    <span>
-                      <img src={iconnheart} alt="Heart Icon" />
-                      Compare
-                    </span>
-                  </li>
-                  <li className="list-inline-item ">
-                    <span>
-                      <img src={iconnheart} alt="Wishlist Icon" />
-                      Wishlist
-                    </span>
-                  </li>
-                  <li className="list-inline-item ">
-                    <span>
-                      <img src={cartimage} alt="Cart Icon" />
-                      Cart
-                    </span>
-                  </li>
-                  <li className="list-inline-item ">
-                    <span onClick={() => setIsopendropdown(!isopendropdown)}>
-                      <img src={user} alt="User Icon" />
-                      Account
-                    </span>
-                    {isopendropdown && (
-                      <ul className="dropdownmenu">
-                        <li>
-                          <Button>
-                            <Person2OutlinedIcon />
-                            My Account
-                          </Button>
-                        </li>
-                        <li>
-                          <Button>
-                            <LocationSearchingOutlinedIcon />
-                            Order tracking
-                          </Button>{" "}
-                        </li>
-                        <li>
-                          <Button>
-                            <SettingsOutlinedIcon />
-                            Setting
-                          </Button>
-                        </li>
-                        <li>
-                          <Button>
-                            <LoginOutlinedIcon />
-                            Log out
-                          </Button>
-                        </li>
-                      </ul>
-                    )}
-                  </li>
-                </ul>
-              </ClickAwayListener>
             </div>
           </div>
         </div>
-      </div>
-    </header>
-    <Nav/>
-   </>
+      </header>
+      <Nav />
+    </>
   );
 };
 
